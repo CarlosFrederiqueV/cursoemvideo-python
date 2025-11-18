f45b73059f74b0dc6757c528297e3ba961d5203f
Carlos Frederique <carlosfvsouza@hotmail.com>
2025-11-18 18:57:00 -0300

    ex005

---
+++ ex005/ex01.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número:'))
+a = n-1
+s = n+1
+print('Analisando o número {}, seu antecessor é {}, e seu sucessor é {}'.format(n , a , s))
+
+
+n = int(input('Digite um número:'))
+print('Analisando o número {}, seu antecessor é {} , e seu sucessor é {}'.format(n, (n-1), (n+1)))
